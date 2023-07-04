a683ab01e39a1c98fe86290459e9d1031b0f963c
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2023-07-04 05:31:05 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (63.85%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int reverse(int x) {
+        long sum =0;
+
+        while(x!=0){
+            int rem = x % 10;
+            sum = sum*10 + rem;
+            x = x/10;
+        }
+      
+       if( sum < Math.pow(-2,31) ||  sum > Math.pow(2,31) ){
+           return 0;
+       }
+       
+       if(x<0){
+           return (int)(-1*sum);
+       }
+       return (int)sum;
+    }
+}
