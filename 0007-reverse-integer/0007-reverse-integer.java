cc3a7666f4a6e260e5805500279c77f2052dc1ec
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2023-07-05 22:01:02 +0530

    Time: 1 ms (100.00%), Space: 40.3 MB (19.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,20 +1,18 @@
 class Solution {
     public int reverse(int x) {
-        long sum =0;
-
-        while(x!=0){
+       long sum =0;
+        
+        while(x != 0){
             int rem = x % 10;
-            sum = sum*10 + rem;
+            sum = sum*10 + rem ;
             x = x/10;
         }
+        if(sum < Math.pow(-2,31) || sum > Math.pow(2,31))
+            return 0;
+        
+        if(x<0)
+            return (int)(-1 * sum);
       
-       if( sum < Math.pow(-2,31) ||  sum > Math.pow(2,31) ){
-           return 0;
-       }
-       
-       if(x<0){
-           return (int)(-1*sum);
-       }
-       return (int)sum;
+        return (int)sum;
     }
 }
