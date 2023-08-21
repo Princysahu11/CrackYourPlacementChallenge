0791afa8f31c51a174131e503111ddc98a175fb8
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2023-08-21 16:51:31 +0530

    Added solution - LeetHub

---
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    
+    {
+        int k =1;
+        Reverse(arr,0,n-k-1);
+        Reverse(arr,n-k,n-1);
+        Reverse(arr,0,n-1);
+        
+    }
+      static void Reverse(int [] arr,int s, int end){
+        while(s<=end){
+            int temp = arr[s];
+            arr[s] = arr[end];
+            arr[end] = temp;
+            s++;
+            end--;
+    
+        }
+    }
+}
