7a23d51aaac898cdd87ecde9163a00fdb1014625
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2023-10-17 10:20:04 +0530

    Time: 1557 ms (53.54%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -8,6 +8,6 @@ IFNULL(ROUND(SUM(p.price*u.units)/sum(u.units),2),0) AS average_price
 from Prices as p LEFT JOIN UnitsSold as u
 
 on p.product_id  = u.product_id 
-AND(  u.purchase_date BETWEEN p.start_date AND p.end_date)
+and(  u.purchase_date BETWEEN p.start_date AND p.end_date)
 group by p.product_id 
 
