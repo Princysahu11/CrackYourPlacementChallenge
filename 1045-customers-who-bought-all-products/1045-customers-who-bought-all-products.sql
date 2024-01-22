17172ef0b59ea86719e7066f6a3f7cffcdde55a6
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2024-01-22 12:57:11 +0530

    Time: 940 ms (80.57%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,5 +3,6 @@
 select cus.customer_id
 from Customer cus 
 group by cus.customer_id
-HAVING COUNT(Distinct product_key) = (Select COUNT(*) from product)
+HAVING COUNT(Distinct product_key) = 
+(Select COUNT(DISTINCT product_key) from product)
 
