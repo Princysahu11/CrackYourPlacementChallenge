341246108978debd7a9962da4bdda9aaef7ee25d
Princy Sahu <95992732+Princysahu11@users.noreply.github.com>
2024-01-25 19:08:38 +0530

    Time: 442 ms (57.28%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,3 +1,4 @@
-select * from cinema 
-where id%2=1 and description<>'boring'
-order by  rating desc
+select id,movie,description,rating
+from Cinema 
+where id%2=1 AND description <> 'boring'
+order by rating DESC
